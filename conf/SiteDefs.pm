2d5695fdbc2c143cdf5266602a9c607160f72f06
Richard Challis <rjchallis@gmail.com>
2019-06-14 15:26:44 +0100

    restore missing pficus

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -19,7 +19,7 @@ sub update_conf {
 
   $SiteDefs::ASSEMBLY_GROUP_A = [
           'Planococcus_citri_pcitriv1',
-          'Planococcus_citri_pcitriv0',
+          'Planococcus_ficus_pficusv0',
           'Pseudococcus_longispinus_v1'
   ];
   $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Mealybug assemblies';
