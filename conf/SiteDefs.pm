50b81593abcc2393f374e93499b8971235ddd690
Richard Challis <rjchallis@gmail.com>
2019-06-14 14:34:20 +0100

    add more assemblies

--- conf/SiteDefs.pm
+++ conf/SiteDefs.pm
@@ -4,13 +4,13 @@ use strict;
 
 
 sub update_conf {
-  push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/gh-ensembl-plugin/modules';
+  push @$SiteDefs::ENSEMBL_API_LIBS, $SiteDefs::ENSEMBL_SERVERROOT . '/ensembl.mealybug/modules';
 
-  $SiteDefs::SITE_LOGO = 'ensembl.genomehubs.png';
+  $SiteDefs::SITE_LOGO = 'mealybugbase.png';
   $SiteDefs::SITE_LOGO_WIDTH = 150;
   $SiteDefs::SITE_LOGO_HEIGHT = 50;
-  $SiteDefs::SITE_LOGO_ALT = 'GenomeHubs Ensembl';
-  $SiteDefs::SITE_NAME = 'GenomeHubs Ensembl';
+  $SiteDefs::SITE_LOGO_ALT = 'Mealybug Ensembl';
+  $SiteDefs::SITE_NAME = 'Mealybug Ensembl';
 
   $SiteDefs::ISSUE_TRACKER_URL = 'https://github.com/genomehubs/genomehubs/issues?status=new&status=open';
   $SiteDefs::ISSUE_TRACKER_TITLE = 'report an issue';
@@ -20,22 +20,27 @@ sub update_conf {
   $SiteDefs::ASSEMBLY_GROUP_A = [
           'Planococcus_citri_pcitriv1',
           'Planococcus_citri_pcitriv0',
-          'Acyrthosiphon_pisum',
-          'Rhodnius_prolixus'
+          'Pseudococcus_longispinus_v1'
   ];
-  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Assemblies with gene models';
+  $SiteDefs::ASSEMBLY_GROUP_A_TITLE = 'Mealybug assemblies';
   $SiteDefs::ASSEMBLY_GROUP_A_TEMPLATE = '_fav_template';
   $SiteDefs::ASSEMBLY_GROUP_B = [
           'Maconellicoccus_hirsutus_mhir1',
           'Ferrisia_virgata_fvir1',
-          'Pseudococcus_longispinus_plon1',
           'Paracoccus_marginatus_pmar1',
           'Trionymus_perrisii_tper1'
   ];
-  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Assembly only';
+  $SiteDefs::ASSEMBLY_GROUP_B_TITLE = 'Mealybug assemblies without gene models';
   $SiteDefs::ASSEMBLY_GROUP_B_TEMPLATE = '_list_template';
+  $SiteDefs::ASSEMBLY_GROUP_C = [
+          'Acyrthosiphon_pisum',
+          'Bemisia_tabaci_MEAM1_v1x1',
+          'Rhodnius_prolixus',
+          'Myzus_persicae_CloneG006_v2'
+  ];
+  $SiteDefs::ASSEMBLY_GROUP_C_TITLE = 'Other Hemiptera';
+  $SiteDefs::ASSEMBLY_GROUP_C_TEMPLATE = '_fav_template';
 }
 
 
 1;
-
--- htdocs/00_set_search_host.js
+++
@@ -1,1 +0,0 @@
-var SEARCH_HOST = 'http://search.mealybug.org';
